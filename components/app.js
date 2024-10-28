0c4cac789c85f2897c1555d2f8e190ee9f0b31ee
Abdul Arif <abdul.arif0705@gmail.com>
2024-10-28 16:50:28 -0400

    comments

--- components/app.js
+++ components/app.js
@@ -5,9 +5,10 @@ import House from "./house";
 
 const App = () => {
   const [selectedHouse, setSelectedHouse] = useState();
-
+  // Reason we make a wrapper. Maybe they will accidentally won't put in a house object, but another object that is not related or some string or number
+  // wrapper function that accepts a house object and does the call to setSelectedHouse. And instead of passing setSelectedHouse to HouseList, we now pass the wrapper to it. The advantage of this approach is that the App component now still is in full control of its own state
   const setSelectedHouseWrapper = (house) => {
-    //do checks on house
+    // check if house object
     setSelectedHouse(house);
   };
 
@@ -19,8 +20,11 @@ const App = () => {
       {selectedHouse ? (
         <House house={selectedHouse} />
       ) : (
-        <HouseList selectHouse={setSelectedHouseWrapper} />
+        <HouseList
+          selectHouse={setSelectedHouseWrapper}
+        /> /* In JS, functions are objects, so they can be passed to other components like any other type  */
       )}
+      {/* If selectedHouse has a truthy value we display the house otherwise by default we display the list */}
     </>
   );
 };
